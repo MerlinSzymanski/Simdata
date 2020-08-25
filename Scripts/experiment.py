bb955ed12b2609b51e191e101acb5b314e51d2ea
Merlin Szymanski <merlin_szymanski@eva.mpg.de>
2020-08-25 15:48:21 +0200

    Add the experiment to the script
    After creating the Datasets the program now runs the \n experiments based on the settings provided in the settings.py file in the scripts module

--- Scripts/experiment.py
+++ Scripts/experiment.py
@@ -1,46 +1,68 @@
 from pathlib import Path
+import subprocess
 import os
-import settings
+from . import settings
 
-def run_megan(dataset, wdir, index):
-    os.mkdir("MEGAN")
-    orders1 = ["snakemake","--jobs", "48", "-s", "/mnt/scratch/merlin/MEGAN_pipeline/metagen.p1.SnakeFile",
-                    "--config", f"bamfile={dataset.absolute()}",  f"byfile={index.absolute()}"]
-    orders2 = ["snakemake","--jobs", "48","-s", "/mnt/scratch/merlin/MEGAN_pipeline/metagen.p2.SnakeFile"]
-    orders3 = ["snakemake","--jobs", "48","-s", "/mnt/scratch/merlin/MEGAN_pipeline/metagen.p3.SnakeFile"]
 
-    print("Run: ", " ".join(orders1))
+def run_megan(dataset, index, wdir):
+    cwd = os.getcwd()
+    dataset = dataset.absolute()
+    index = index.absolute()
+    wdir.mkdir(exist_ok=True, parents=True)
+    os.chdir(wdir.absolute())
+
+    orders1 = ["snakemake", "--jobs", "48", "-s", settings.SNAKEFILE1, "--config",
+               f"bamfile={str(dataset)}", f"byfile={str(index)}"]
+    orders2 = ["snakemake", "--jobs", "48", "-s", settings.SNAKEFILE2]
+    orders3 = ["snakemake", "--jobs", "48", "-s", settings.SNAKEFILE3]
+
+    print("Run: ", " ".join([str(x) for x in orders1]))
     subprocess.run(orders1)
-    print("Run: ", " ".join(orders2))
+    print("Run: ", " ".join([str(x) for x in orders2]))
     subprocess.run(orders2)
-    print("Run: ", " ".join(orders3))
+    print("Run: ", " ".join([str(x) for x in orders3]))
     subprocess.run(orders3)
 
+    os.chdir(cwd)
+    return True
 
-def run_kraken(dataset, wdir, kmer, index):
+
+def run_kraken(dataset, index, wdir, kmer, kraken_filter):
+    dataset = dataset.absolute()
+    index = index.absolute()
+
     nextflow = settings.NEXTFLOW
     kmerdir = settings.KMERDIR
     genomes = settings.GENOMES
-    
-    cwd = os.getcwd() 
-    os.chdir(wdir.joinpath(f"Kmer{kmer}").mkdir(exists_ok=True))
-    
-    orders = ["nextflow", "run", "-resume", nextflow, "--rg", index.absolute(), "--bam", 
-            dataset.absolute(), "--db", f"{kmerdir}/Mito_db_Kmer{kmer}", "--genome", 
-            genomes]
-    print(f"Running:{' '.join(orders)}")
+
+    cwd = os.getcwd()
+    wdir.mkdir(exist_ok=True, parents=True)
+    os.chdir(wdir.absolute())
+
+    orders = ["nextflow", "run", "-resume", nextflow, "--rg", str(index), "--bam",
+              str(dataset), "--db", f"{kmerdir}/Mito_RefSeqRel97_Kmer{kmer}", "--genome",
+              genomes]
+
+    if kraken_filter > 0:
+        orders.extend(["--krakenfilter", kraken_filter])
+
+    print(f"Running:{' '.join([str(x) for x in orders])}")
     subprocess.run(orders)
 
     os.chdir(cwd)
     return True
 
-def main(ds_id, regime):
-    dataset = Path(f"Datasets/Dataset_{ds_id}_{regime}")
-    index = Path(f"Datasets/indexfile.tsv")
-    wdir = Path(f"Experiments/Dataset{ds_id}/Regime{regime}/").mkdir(exists_ok=True, parents=True)
-    for kmer in settings.KMERS:
-        run_kraken(dataset, wdir, kmer, index)
-    
-    run_megan()
-    return True
 
+def main(ds_id, regime, kmer, kraken_filter):
+    dataset = Path(f"Datasets/Dataset_{ds_id}_{regime}.sam")
+    index = Path(f"Datasets/indexfile.tsv")
+
+    if kmer != "MEGAN":
+        wdir = Path(f"Experiments/Dataset{ds_id}/Regime{regime}/Kmer{kmer}/kraken_filter_{kraken_filter}")
+        run_kraken(dataset, index, wdir, kmer, kraken_filter)
+
+    else:
+        wdir = Path(f"Experiments/Dataset{ds_id}/Regime{regime}/MEGAN")
+        run_megan(dataset, index, wdir)
+
+    return True
--- Scripts/settings.py
+++ Scripts/settings.py
@@ -1,1 +1,13 @@
+import os
+
 EMAIL = "merlin.szymanski@gmail.com"
+
+NEXTFLOW = "/home/merlin_szymanski/.nextflow/assets/merlin/sediment_nf/main.nf"
+KMERDIR = "/mnt/scratch/merlin/Kraken/"
+KMERS = [18, 19, 20, 22, 24, 28, 30, "MEGAN"]
+KRFILTER = [0, 0.1, 0.3, 0.5]
+GENOMES = "/mnt/sequencedb/Refseq/refseq_mammalian_mt_rel97/"
+
+SNAKEFILE1 = "/mnt/scratch/merlin/MEGAN_pipeline/metagen.p1.SnakeFile"
+SNAKEFILE2 = "/mnt/scratch/merlin/MEGAN_pipeline/metagen.p2.SnakeFile"
+SNAKEFILE3 = "/mnt/scratch/merlin/MEGAN_pipeline/metagen.p3.SnakeFile"
