6226c858790e28335405db7d8796b798a874985b
Merlin Szymanski <merlin_szymanski@eva.mpg.de>
2020-08-29 02:16:10 +0200

    Bugfix Summaryscript

--- Scripts/summary.py
+++ Scripts/summary.py
@@ -58,10 +58,15 @@ class Sequence:
 
 
 def extract_records_from_bam(samfile):
-    return [x.split("\t")[0] for x in open(samfile, "r")]
+    try:
+        return [x.split("\t")[0] for x in open(samfile, "r")]
+    except:
+        pysam.index(str(samfile))
+        return [x.query_name for x in pysam.AlignmentFile(samfile, check_sq=False)]
 
 
 def summarize_megan(location, records, ds_id, regime):
+    print(f"Summarize MEGAN: Dataset{ds_id}, Regime{regime}")
     rec_seq_dict = {rec: Sequence(rec, ds_id, regime, "MEGAN", 0) for rec in records}
     run_dir = location.joinpath(f"MEGAN")
 
@@ -69,33 +74,35 @@ def summarize_megan(location, records, ds_id, regime):
         extracted_bam = [x for x in family.glob("*.bam")]
         if len(extracted_bam) < 1:
             continue
-        for record in extract_records_from_bam(extracted_bam):
-            rec_seq_dict[record]["extracted"] = True
-            rec_seq_dict[record]["assigned_family"] = family.name
+        for record in extract_records_from_bam(extracted_bam[0]):
+            rec_seq_dict[record].extracted = True
+            rec_seq_dict[record].assigned_family = family.name
         for aligned_bam in [x for x in family.joinpath("aligned").glob("*.bam")]:
             for record in extract_records_from_bam(aligned_bam):
-                rec_seq_dict[record]["aligned"] = True
+                rec_seq_dict[record].aligned = True
 
     return list(rec_seq_dict.values())
 
 
 def summarize_kraken(location, records, ds_id, regime, kmer, krf):
+    print(f"Summarize Kraken: Dataset{ds_id}, Regime{regime}, Kmer{kmer}, Krakenfilter {krf}")
+    print(f"Records in kraken: {len(records)}")
     rec_seq_dict = {rec: Sequence(rec, ds_id, regime, kmer, krf) for rec in records}
     run_dir = location.joinpath(f"Kmer{kmer}").joinpath(f"kraken_filter_{krf}")
-
+    print(f"ref_seq_dict: {len(rec_seq_dict)}")
     for family in run_dir.joinpath("out").iterdir():
         extracted_bam = [x for x in family.glob("*.bam")]
         if len(extracted_bam) < 1:
             continue
-        for record in extract_records_from_bam(extracted_bam):
-            rec_seq_dict[record]["extracted"] = True
-            rec_seq_dict[record]["assigned_family"] = family.name
+        for record in extract_records_from_bam(extracted_bam[0]):
+            rec_seq_dict[record].extracted = True
+            rec_seq_dict[record].assigned_family = family.name
         for aligned_bam in [x for x in family.joinpath("aligned").glob("*[!_deduped].bam")]:
             for record in extract_records_from_bam(aligned_bam):
-                rec_seq_dict[record]["aligned"] = True
+                rec_seq_dict[record].aligned = True
         for bedfiltered_bam in [x for x in family.joinpath("bedfiltered").glob("*.bam")]:
             for record in extract_records_from_bam(bedfiltered_bam):
-                rec_seq_dict[record]["bedfiltered"] = True
+                rec_seq_dict[record].bedfiltered = True
 
     return list(rec_seq_dict.values())
 
@@ -124,26 +131,29 @@ def main(ds_id, regime, dataset_dir="Datasets", savedir="Summary", experiment_di
     all_seqs = []
 
     records = [x.split("\t")[0] for x in open(dataset_dir.joinpath(f"Dataset_{ds_id}_{regime}.sam"), "r")]
+    print(f"Records:{len(records)}")
     location = experiment_dir.joinpath(f"Dataset{ds_id}").joinpath(f"Regime{regime}")
 
     for kmer in kmers:
         if kmer == "MEGAN":
             seqs = summarize_megan(location, records, ds_id, regime)
+            print(len(seqs))
             all_seqs.extend(seqs)
         else:
             for krf in kfilter:
                 seqs = summarize_kraken(location, records, ds_id, regime, kmer, krf)
+                print(len(seqs))
                 all_seqs.extend(seqs)
 
-    with open(savedir.joinpath(f"Summary_Dataset{ds_id}_Regime{regime}.tsv")) as outfile:
+    savedir.mkdir(exist_ok=True, parents=True)
+    with open(savedir.joinpath(f"Summary_Dataset{ds_id}_Regime{regime}.tsv"), "w") as outfile:
         outfile.write(
             "Description\tAccession\tDataset\tRegime\tKmer\tKrakenfilter\tFamily\t"
             "Assigned_Fam\tExtracted\tAligned\tBedfiltered\n"
         )
         for seq in all_seqs:
-            seq.get_taxonomy_from_accession()
             outfile.write(
-                f"{seq.description}\t{seq.accession}\t{seq.dataset}\t{seq.regime}\t{seq.kmer}\t{seq.kraken_filter}\t"
+                f"{seq.description}\t{seq.get_accession_from_description()}\t{seq.dataset}\t{seq.regime}\t{seq.kmer}\t{seq.kraken_filter}\t"
                 f"{seq.original_family}\t{seq.assigned_family}\t{seq.extracted}\t{seq.aligned}\t{seq.bedfiltered}\n"
             )
     return True
